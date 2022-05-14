de717e2cfc722abfc61ec03b1b071b2186e48bee
Shivam Bhardwaj <63858386+bshivam2001@users.noreply.github.com>
2022-05-14 20:32:10 +0530

    Made game_stats.py module independent of the game

--- game_stats.py
+++ game_stats.py
@@ -1,21 +1,18 @@
 __STATS_FILE__ = 'stats.txt'
 
-# STAT DICT
-wordle_stats = {
-    'SUCCESSES': 0,
-    'FAILURES': 0,
-    'ERRORS': 0,
-    '1TRY': 0,
-    '2TRY': 0,
-    '3TRY': 0,
-    '4TRY': 0,
-    '5TRY': 0,
-    '6TRY': 0
-}
+# Store all different types of stats you'd like to save
+available_stats = ['SUCCESSES', 'FAILURES', 'ERRORS', '1TRY', '2TRY', '3TRY', '4TRY', '5TRY', '6TRY']
+
+# Dictionary to store stat values
+game_stats = {}
+# Initialize dictionary
+for stat in available_stats:
+    game_stats[stat] = 0
+
 
 #Function to read the stats file
 def read_stats():
-    global wordle_stats
+    global game_stats
     try:
         with open('stats.txt') as file:
             lines = file.readlines()
@@ -27,8 +24,8 @@ def read_stats():
                 elif len(line) > 2:
                     continue
                 
-                if line[0] in wordle_stats.keys():
-                    wordle_stats[line[0]] = int(line[1])
+                if line[0] in game_stats.keys():
+                    game_stats[line[0]] = int(line[1])
                  
     except FileNotFoundError:
         print('Creating stats file...')
@@ -38,16 +35,30 @@ def read_stats():
         print('Error parsing stats file')
 
 
+# Function to increment the value of the given stat
+def increment_stat(stat):
+    if stat in game_stats.keys():
+        game_stats[stat] += 1
+    else:
+        print('ERROR: Statistic value {stat} not available')
+
+
 # Function to update the current stats in the file
 def update_stats():
-    global wordle_stats
+    global game_stats
     with open(__STATS_FILE__, 'w') as file:
-        for key, value in wordle_stats.items():
+        for key, value in game_stats.items():
             file.write(str(key + '=' + str(value) + '\n'))
 
 
 def show_stats():
-    total_games = wordle_stats['SUCCESSES'] + wordle_stats['FAILURES']
-    total_games = 1 if total_games == 0 else total_games # Avoid division by zero
-    success_rate = (wordle_stats['SUCCESSES'] / total_games) * 100
-    print('Success Rate: %.2f %%' % (success_rate))
+    # Show success rate if available
+    try:
+        total_games = game_stats['SUCCESSES'] + game_stats['FAILURES']
+        total_games = 1 if total_games == 0 else total_games # Avoid division by zero
+        success_rate = (game_stats['SUCCESSES'] / total_games) * 100
+        print('\nSuccesses: %s' % (game_stats['SUCCESSES']))
+        print('Failures: %s' % (game_stats['FAILURES']))
+        print('Success Rate: %.2f %%' % (success_rate))
+    except KeyError:
+        pass
